d6154c9e2f8d5e14f11e284253814737a57f1fd5
Gustavo Henrique <gustavohpereira1.6@gmail.com>
2024-02-06 19:53:29 -0300

    update app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,22 @@
+import logo from './assets/Logo.svg'
+import { GridNoteCard } from './components/note-card/grid-note-card'
+
 export function App() {
-  return <h1>Hello World</h1>
+  return (
+    <div className="mx-auto my-12 max-w-6xl space-y-6">
+      <img src={logo} alt="Logo do notes" />
+
+      <form className="w-full">
+        <input
+          className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500"
+          type="text"
+          placeholder="Busque em suas notas..."
+        />
+      </form>
+
+      <div className="h-px bg-slate-700" />
+
+      <GridNoteCard />
+    </div>
+  )
 }
