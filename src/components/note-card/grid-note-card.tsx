1f058159c4e51ecc7a1ba1d193532ab5f689cd98
Gustavo Henrique <gustavohpereira1.6@gmail.com>
2024-02-07 17:59:00 -0300

    update note-card

--- src/components/note-card/grid-note-card.tsx
+++ src/components/note-card/grid-note-card.tsx
@@ -6,22 +6,22 @@ export function GridNoteCard() {
     <div className="grid auto-rows-[250px] grid-cols-3 gap-6">
       <NewNoteCard />
       <NoteCard
-        title="há 1 dia"
-        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci error praesentium dolorum! Itaque voluptatem quae voluptate esse labore minus omnis dolorem! Provident perferendis saepe error itaque aspernatur nulla illum quo."
+        date={new Date()}
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci error praesentium dolorum! Itaque voluptatem quae voluptate esse labore minus omnis dolorem! Provident perferendis saepe error itaque aspernatur nulla illum quo."
       />
       <NoteCard
-        title="há 3 dia"
-        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni quas vitae perspiciatis aperiam, modi laborum libero! Magnam ipsam minus optio tenetur fuga quae praesentium soluta placeat, veniam, eius tempora doloribus?"
-      />
-
-      <NoteCard
-        title="há 4 dia"
-        description="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum perspiciatis non nihil harum dolor maiores quia, quaerat possimus voluptate natus? Distinctio itaque ad sequi odio nostrum. Officia repudiandae aspernatur sit."
+        date={new Date()}
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni quas vitae perspiciatis aperiam, modi laborum libero! Magnam ipsam minus optio tenetur fuga quae praesentium soluta placeat, veniam, eius tempora doloribus?"
       />
 
       <NoteCard
-        title="há 5 dia"
-        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor expedita consequuntur eligendi obcaecati dignissimos numquam ratione mollitia, est perspiciatis aspernatur id harum corporis voluptatem quod in labore cumque blanditiis! Aliquid."
+        date={new Date()}
+        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum perspiciatis non nihil harum dolor maiores quia, quaerat possimus voluptate natus? Distinctio itaque ad sequi odio nostrum. Officia repudiandae aspernatur sit."
+      />
+
+      <NoteCard
+        date={new Date()}
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor expedita consequuntur eligendi obcaecati dignissimos numquam ratione mollitia, est perspiciatis aspernatur id harum corporis voluptatem quod in labore cumque blanditiis! Aliquid."
       />
     </div>
   )
